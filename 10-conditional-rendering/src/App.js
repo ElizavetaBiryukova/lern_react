4a5130c18ebf69bdcfb323ed02a4995e7f3d76fb
Elizaveta Biryukova <birliza@mail.ru>
2023-10-05 13:36:43 +0200

    feat: add conditional rendering

---
+++ 10-conditional-rendering/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import PetInfo from './components/PetInfo';
+
+function App() {
+  return (
+    <div className="App">
+      <PetInfo
+        animal="cat"
+        age={5}
+        hasPet
+      />
+      <PetInfo
+        animal="dog"
+        age={7}
+        hasPet={false}
+      />
+    </div>
+  );
+}
+
+export default App;
