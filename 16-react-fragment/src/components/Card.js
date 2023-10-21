93ebf5eab6af24775ee306420ef5f99a5e0ad9b1
Elizaveta Biryukova <birliza@mail.ru>
2023-10-21 15:23:30 +0200

    feat: add 16-react-fragment

---
+++ 16-react-fragment/src/components/Card.js
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <>
+            <h1>Liza</h1>
+            <h3>Hello</h3>
+            <button>Like!</button>
+        </>
+    )
+}
+
+export default Card
