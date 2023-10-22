b8bfe4f3b63ff1376dec70cb1325319fc85d9d46
Elizaveta Biryukova <birliza@mail.ru>
2023-10-22 21:44:03 +0200

    feat: add 19-children-prop

---
+++ 19-children-prop/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Wrapper from './components/Wrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper color='lightblue'><h2>Text wrapper</h2>
+        <button>Click</button>
+      </Wrapper>
+      <Wrapper color='lightgreen'><h2>Text wrapper</h2>
+        <p>some description</p>
+        <input type="text" placeholder="Enter value" />
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
